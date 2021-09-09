29693fc1d3a4ed0a4bb470389e2ff088101f7732
why520crazy <why520crazy@163.com>
2021-09-09 18:42:03 +0800

    feat: add prebump and postbump hooks

--- src/lifecycles/bump.ts
+++ src/lifecycles/bump.ts
@@ -12,7 +12,9 @@ import standardVersion = require('standard-version');
 
 export class BumpLifecycle extends Lifecycle {
     async run(context: CommandContext): Promise<void> {
+        
         this.logger.info(`using standard-version to bumping, outputting changes and commit`);
+        await this.runLifecycleHook('prebump', context.options, this.getLifecycleHookParams(context));
         const options: standardVersion.Options = Object.assign({}, context.options, {
             releaseAs: context.versions.next
         }) as standardVersion.Options;
@@ -23,5 +25,6 @@ export class BumpLifecycle extends Lifecycle {
         // 2. generate changelog
         // 3. git commit
         await standardVersion(options);
+        await this.runLifecycleHook('postbump', context.options, this.getLifecycleHookParams(context));
     }
 }
