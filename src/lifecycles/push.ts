08bff76e8f8d2554637dabc3d47c34f11f66dab2
zhaoyunfeng <zhaoyunfeng@worktile.com>
2022-03-30 13:52:50 +0800

    feat: now push lifecycle prompts pr url

--- src/lifecycles/push.ts
+++ src/lifecycles/push.ts
@@ -14,14 +14,26 @@ export class PushLifecycle extends Lifecycle {
         if (context.options.skip.push) {
             return;
         }
+
+        const remote = 'origin';
         const branch = context.releaseBranch || context.currentBranch;
-        this.logger.info(`pushing ${chalk.green(branch)} to remote`);
+        this.logger.info(`pushing ${chalk.green(branch)} to remote ${chalk.green(remote)}`);
+
         if (!context.options.dryRun && branch) {
             context.git.outputHandler((command, stdout, stderr) => {
                 stdout.pipe(process.stdout);
                 stderr.pipe(process.stderr);
             });
-            await context.git.push('origin', branch);
+            await context.git.push(remote, branch);
+
+            // prompt to create PR based correct branch
+            const remoteUrl = await context.git.remote(['get-url', remote, '--push']) as string;
+            const githubHostname = 'github.com';
+            if (remoteUrl.includes(githubHostname)) {
+                const repoHttpsUrl = `https://${githubHostname}/${remoteUrl.slice(remoteUrl.indexOf(githubHostname) + githubHostname.length + 1, remoteUrl.lastIndexOf('.git'))}`;
+                const createPRUrl = `${repoHttpsUrl}/pull/new/${context.currentBranch}..${branch}`;
+                this.logger.info(`Create a pull request: ${createPRUrl}`);
+            }
         }
     }
 }
