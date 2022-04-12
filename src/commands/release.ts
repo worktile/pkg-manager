dae1ea8558a19217adf1163fe7f1aeb1cfa989b0
why520crazy <why520crazy@163.com>
2022-04-12 19:53:31 +0800

    feat: update releaseCommitMessageFormat to "build: release v{{version}}"

--- src/commands/release.ts
+++ src/commands/release.ts
@@ -6,78 +6,80 @@
  * found in the LICENSE file at https://github.com/worktile/pkg-manager/blob/master/LICENSE
  */
 
-import { CommandModule } from "yargs";
-import _ from "lodash";
-import { CommandOptions } from "../interface";
-import { defaults } from "../defaults";
-import { ReleaseHandler } from "../handlers";
-import { logger } from "../logger";
+import { CommandModule } from 'yargs';
+import _ from 'lodash';
+import { CommandOptions } from '../interface';
+import { defaults } from '../defaults';
+import { ReleaseHandler } from '../handlers';
+import { logger } from '../logger';
 
 export const releaseCommand: CommandModule = {
-  command: ["release [releaseAs]"],
-  describe:
-    "Release package or project contains bump version, changelog, create release branch, push",
-  builder: (yargs) => {
-    yargs
-      .option("skip", {
-        alias: "s",
-        // choices: ['bump', 'changelog', 'commit', 'branch', 'push'],
-        desc: `Map of steps in the release process that should be skipped`,
-        default: defaults.skip,
-      })
-      .option("release-branch-format", {
-        desc: `Release branch format`,
-        default: defaults.releaseBranchFormat,
-      })
-      .option("bump-files", {
-        default: defaults.bumpFiles,
-        desc: `List of files in the release bump process`,
-        array: true,
-      })
-      .option("preset", {
-        default: defaults.preset,
-        desc: `Commit message guideline preset, see https://github.com/conventional-changelog/conventional-changelog`,
-        string: true,
-      })
-      .option("commit-all", {
-        alias: "a",
-        default: defaults.commitAll,
-        desc: `Commit all staged changes, not just files affected by wpm`,
-        boolean: true,
-      })
-      .option("infile", {
-        alias: "i",
-        describe: "Read the CHANGELOG from this file",
-        default: defaults.infile,
-      });
+    command: ['release [releaseAs]'],
+    describe: 'Release package or project contains bump version, changelog, create release branch, push',
+    builder: (yargs) => {
+        yargs
+            .option('skip', {
+                alias: 's',
+                // choices: ['bump', 'changelog', 'commit', 'branch', 'push'],
+                desc: `Map of steps in the release process that should be skipped`,
+                default: defaults.skip
+            })
+            .option('release-branch-format', {
+                desc: `Release branch format`,
+                default: defaults.releaseBranchFormat
+            })
+            .option('release-commit-message-format', {
+                desc: `Release commit message format`,
+                default: defaults.releaseCommitMessageFormat
+            })
+            .option('bump-files', {
+                default: defaults.bumpFiles,
+                desc: `List of files in the release bump process`,
+                array: true
+            })
+            .option('preset', {
+                default: defaults.preset,
+                desc: `Commit message guideline preset, see https://github.com/conventional-changelog/conventional-changelog`,
+                string: true
+            })
+            .option('commit-all', {
+                alias: 'a',
+                default: defaults.commitAll,
+                desc: `Commit all staged changes, not just files affected by wpm`,
+                boolean: true
+            })
+            .option('infile', {
+                alias: 'i',
+                describe: 'Read the CHANGELOG from this file',
+                default: defaults.infile
+            });
 
-    return yargs;
-  },
-  handler: async (argv: any) => {
-    if (!_.isObject(argv.skip)) {
-      return logger.warn(
-        `--skip args(${argv.skip}) is invalid, please use --skip.bump --skip.changelog`
-      );
+        return yargs;
+    },
+    handler: async (argv: any) => {
+        if (!_.isObject(argv.skip)) {
+            return logger.warn(`--skip args(${argv.skip}) is invalid, please use --skip.bump --skip.changelog`);
+        }
+        const options: CommandOptions = {
+            dryRun: argv.dryRun,
+            skip: argv.skip,
+            defaultBranch: argv.defaultBranch,
+            allowBranch: argv.allowBranch,
+            releaseBranchFormat: argv.releaseBranchFormat,
+            releaseCommitMessageFormat: argv.releaseCommitMessageFormat,
+            releaseAs: argv.releaseAs,
+            bumpFiles: argv.bumpFiles,
+            issueUrlFormat: argv.issueUrlFormat,
+            preset: argv.preset,
+            commitAll: argv.commitAll,
+            hooks: argv.hooks,
+            infile: argv.infile,
+            tagPrefix: argv.tagPrefix,
+            cwd: defaults.cwd
+        };
+        const handler = new ReleaseHandler(options);
+        await handler.start().catch((error) => {
+            logger.error(error);
+        });
     }
-    const options: CommandOptions = {
-      dryRun: argv.dryRun,
-      skip: argv.skip,
-      defaultBranch: argv.defaultBranch,
-      allowBranch: argv.allowBranch,
-      releaseBranchFormat: argv.releaseBranchFormat,
-      releaseAs: argv.releaseAs,
-      bumpFiles: argv.bumpFiles,
-      issueUrlFormat: argv.issueUrlFormat,
-      preset: argv.preset,
-      commitAll: argv.commitAll,
-      hooks: argv.hooks,
-      infile: argv.infile,
-      tagPrefix: argv.tagPrefix,
-      cwd: defaults.cwd,
-    };
-    const handler = new ReleaseHandler(options);
-    await handler.start().catch((error) => {
-      logger.error(error);
-    });
-  },
 };
